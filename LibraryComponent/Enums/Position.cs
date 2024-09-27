c84df5da73f70d13a487a23517c30d11813724d3
CodingHub <95412136+codinghubytb@users.noreply.github.com>
2024-09-27 18:02:01 +0200

    Add Components

---
+++ LibraryComponent/Enums/Position.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryComponent.Enums
+{
+    public enum Position
+    {
+        Center,
+        NorthWest,
+        NorthEast,
+        SouthEast,
+        SouthWest
+    }
+}
